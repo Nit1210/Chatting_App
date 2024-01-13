bab3d84e774de43a9fe88dd38828ccee17cebaea
Nit1210 <98055206+Nit1210@users.noreply.github.com>
2024-01-12 21:21:22 -0500

    UI changes

--- Scripts/ChatUI.js
+++ Scripts/ChatUI.js
@@ -9,15 +9,37 @@ export class chatUI{
     }
     render(data){
         const when =formatDistanceToNow(data.created_at.toDate(),{addSuffix:true});
-       if(data.filename){
+         if(localStorage.getItem('username')===data.username&& data.filename){
+            this.List.innerHTML+=`
+            <li class="list-group-item curUserMsg" >
+            <span class="username">${data.username}: </span>
+            <br>
+            <a class="filename" href="${data.path}">${data.filename}</a>
+            <div class="filesize">Size:${data.size}</div>
+            <div class="time-right">${when}</div>
+          </li>`
+        }
+       else if(data.filename){
            this.List.innerHTML+=`
-           <li class="list-group-item" >
+           <li class="list-group-item fileMessage" >
            <span class="username">${data.username}: </span>
            <br>
            <a class="filename" href="${data.path}">${data.filename}</a>
            <div class="filesize">Size:${data.size}</div>
            <div class="time">${when}</div>
          </li>`
+       }
+     
+       else if(localStorage.getItem('username')===data.username&&!data.filename){
+        this.List.innerHTML+=`
+        <li class="list-group-item curUserMsg" >
+          <span class="username">${data.username}: </span>
+          <br>
+          <span class="message">${data.message}</span>
+          <div class="time-right">${when}</div>
+        </li>
+        `;
+
        }
        else{
         this.List.innerHTML+=`
